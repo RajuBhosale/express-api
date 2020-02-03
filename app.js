d2994901d0a14d184a95bdf10acc1cba131775c2
RajuBhosale <raju@blimp.co.in>
2020-02-03 18:14:46 +0530

    Eslint fixes

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-var envVar = require('dotenv').config();
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors')
+const envVar = require('dotenv').config();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors')
 
-var log = require('./utils/logger');
+const log = require('./utils/logger');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-if(envVar) {
+if (envVar) {
   log.info('Environment config loaded');
 }
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+/* eslint-disable new-cap */
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express again' });
+  res.render('index', {title: 'Express again'});
 });
 
 module.exports = router;
